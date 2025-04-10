5b46182edcd79323955e07933fee55c8e893f15d
Rob Lindsey <rob@roblindsey.com>
2025-04-10 11:24:45 -0400

    changed page titles

---
+++ src/assets/js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const colorPalette = ["#261a1c", "#1f1d14", "#121f1d", "#1a1d26"];
+	const pageHeadline = document.querySelector(".page-intro");
+	let randomNum = Math.floor(Math.random() * 4);
+	pageHeadline.style.backgroundColor = colorPalette[randomNum];
+	console.log("test");
+});
